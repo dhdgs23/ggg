b58e54e78e978d5710a2eebb3023e3b0e1c00eaa
Sayan Mondal <sm187966@gmail.com>
2025-08-14 14:38:45 +0000

    "ReferenceError: DialogTrigger is not defined     at renderContent (http

--- src/components/purchase-modal.tsx
+++ src/components/purchase-modal.tsx
@@ -1,9 +1,10 @@
+
 'use client';
 
 import { useState, useEffect, useCallback } from 'react';
 import { useRouter } from 'next/navigation';
 import Script from 'next/script';
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogTrigger } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
