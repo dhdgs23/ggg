2c5b9919bfdd0b1fd14692226256118ec4e6ea6c
Sayan Mondal <sm187966@gmail.com>
2025-11-09 07:36:13 +0000

    no then crete a new scle or do whatever needed becuse left-16 is not eno

--- src/components/second-card-decoration.tsx
+++ src/components/second-card-decoration.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 export default function SecondCardDecoration() {
   return (
-    <div className="absolute -top-4 -left-16 z-10 w-48 h-48 pointer-events-none">
+    <div className="absolute -top-4 -left-[4.5rem] z-10 w-48 h-48 pointer-events-none">
       <Image
         src="https://res.cloudinary.com/dlvoikod1/image/upload/v1762668141/images__1_-removebg-preview_fylmgh.png"
         alt="Card Decoration"
